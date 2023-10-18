858e3f58e346b1b6aff1fc4bf85832dfb9e17c59
TheIronboy <66006359+TheIronboy@users.noreply.github.com>
2023-10-18 22:44:12 +0200

    first commit (absolutely nothing done, just forms)

---
+++ SSH_Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pi_NAS_Manager
+{
+    public partial class SSH_Login : Form
+    {
+        public SSH_Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Snippets_Manager.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Pi_NAS_Manager
+{
+    partial class Snippets_Manager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Snippets_Manager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 397);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Snippets_Manager";
+            this.Text = "Snippets_Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
